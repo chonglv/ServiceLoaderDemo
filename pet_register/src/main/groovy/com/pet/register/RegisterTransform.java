e2cb9ccd99a3f570ecbe3a61d27514e57912da44
lvchong <lvchong@sogou-inc.com>
2020-05-21 19:28:03 +0800

    add demo

--- pet_register/src/main/groovy/com/pet/register/RegisterTransform.java
+++ pet_register/src/main/groovy/com/pet/register/RegisterTransform.java
@@ -12,11 +12,25 @@ import com.android.build.api.transform.TransformOutputProvider;
 import com.android.build.gradle.internal.pipeline.TransformManager;
 import com.android.utils.FileUtils;
 
+import org.gradle.internal.impldep.org.apache.http.util.TextUtils;
+import org.gradle.util.TextUtil;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.Map;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 
 public class RegisterTransform extends Transform {
 
@@ -50,7 +64,7 @@ public class RegisterTransform extends Transform {
                 System.out.println(jarInput.getContentTypes());
                 System.out.println(jarInput.getScopes());
                 System.out.println(dest.getAbsolutePath());
-
+                scanJar(jarInput.getFile());
                 FileUtils.copyFile(jarInput.getFile(), dest);
             }
             for (DirectoryInput directoryInput : input.getDirectoryInputs()) {
@@ -70,6 +84,89 @@ public class RegisterTransform extends Transform {
         }
 
     }
+
+    static void scanJar(File jarFile) {
+        if (jarFile!=null && jarFile.exists()) {
+            JarFile file = null;
+            try {
+                file = new JarFile(jarFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Enumeration enumeration = file.entries();
+            while (enumeration.hasMoreElements()) {
+                JarEntry jarEntry = (JarEntry) enumeration.nextElement();
+                String entryName = jarEntry.getName();
+                if (entryName.startsWith("com/input/pet/Pet$")) {
+                    System.out.println(entryName);
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = file.getInputStream(jarEntry);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    scanClass(inputStream);
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            try {
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static void scanClass(File file) {
+        try {
+            scanClass(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void scanClass(InputStream inputStream) {
+        ClassReader cr = null;
+        try {
+            cr = new ClassReader(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+        ScanClassVisitor cv = new ScanClassVisitor(Opcodes.ASM5, cw);
+        cr.accept(cv, ClassReader.EXPAND_FRAMES);
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ScanClassVisitor extends ClassVisitor {
+
+        ScanClassVisitor(int api, ClassWriter cv) {
+            super(api, cv);
+        }
+
+        public void visit(int version, int access, String name, String signature,
+                          String superName, String[] interfaces) {
+            super.visit(version, access, name, signature, superName, interfaces);
+            System.out.println("name="+name+" super="+superName+" interfaces="+interfaces);
+            for (String face: interfaces){
+                System.out.println("face="+face);
+                if ("com/input/pet/IPet".equals(face)){
+                    System.out.println("This is:"+name);
+                }
+            }
+        }
+
+
+    }
+
 
 
     @Override
